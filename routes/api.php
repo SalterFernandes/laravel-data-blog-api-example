7bffa0e798961c0d881df5fe87da8202db8a24b6
Sergio Fernandes <salter.sfernandes@gmail.com>
2025-11-17 12:29:16 +0000

    feat: Enabled API routes

---
+++ routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\AuthController;
+use App\Http\Controllers\Api\V1\PostController;
+use App\Http\Controllers\Api\V1\CommentController;
+use App\Http\Controllers\Api\V1\CategoryController;
+
+//Public routes
+Route::prefix('v1')->group(function () {
+
+    // Auth
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+
+});
